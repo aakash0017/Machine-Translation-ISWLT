b78626440e8668a2f0457408b44f55594cc8ad4d
nid989 <nidbhavsar989@gmail.com>
2022-03-19 13:36:28 +0530

    minor changes

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH -N 1 #1 node
-#SBATCH -n 20
+#SBATCH -n 8
 #SBATCH --job-name="isometric_iswlt"
 #SBATCH --output=isometric_iswlt.out
 #SBATCH --error=isometric_iswlt.err
@@ -8,8 +8,7 @@
 #SBATCH --ntasks=8
 #SBACTH
 #SBATCH --mem=16G
-#SBATCH --cpus-per-task=40
 #SBATCH --gres=gpu:1
-#SBATCH --time=0-6:00:00 
+#SBATCH --time=0-06:00:00 
 
 python3 train.py
