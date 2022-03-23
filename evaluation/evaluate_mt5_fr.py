e683891c5ddcde4c495e417a612dae8464e9b782
Aakash Bhatnagar <44599944+aakash0017@users.noreply.github.com>
2022-03-23 19:06:34 +0530

    Update evaluate_mt5_fr.py

--- evaluation/evaluate_mt5_fr.py
+++ evaluation/evaluate_mt5_fr.py
@@ -13,26 +13,6 @@ import os
 import pandas as pd
 
 import boto3
-session = boto3.Session(
-    aws_access_key_id='AKIA4QB2WTN57SCTNAGG',
-    aws_secret_access_key='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',
-)
-s3 = session.resource('s3')
-s3.meta.client.download_file(Bucket='mtacl', Key='fr_en_test_data', Filename='fr_en_test.csv')
-  
-test = pd.read_csv('fr_en_test.csv')
-
-#Remove any possible duplicates
-test = test.drop_duplicates(subset=["en", "fr"])
-
-test = test[["en", "fr"]]
-test.columns = ["input_text", "target_text"]
-
-test['input_text'] = test.progress_apply(
-    lambda row: add_verbosity(row['input_text'], row['target_text']),
-    axis=1
-)
-test = test.astype(str)
 
 def add_verbosity(input, target, test=True):
   ts_ratio = len(target.split(' '))/len(input.split(' '))
@@ -47,13 +27,39 @@ def add_verbosity(input, target, test=True):
       prefix = "long"
   return prefix + " " + input
 
+print('downloading data from s3')
+session = boto3.Session(
+    aws_access_key_id='AKIA4QB2WTN57SCTNAGG',
+    aws_secret_access_key='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',
+)
+
+s3 = session.resource('s3')
+s3.meta.client.download_file(Bucket='mtacl', Key='fr_en_test_data', Filename='fr_en_test.csv')
+ 
+print('donwloaded data')
+ 
+test = pd.read_csv('fr_en_test.csv')
+
+#Remove any possible duplicates
+test = test.drop_duplicates(subset=["en", "fr"])
+
+test = test[["en", "fr"]][2000:]
+test.columns = ["input_text", "target_text"]
+
+test['input_text'] = test.apply(
+    lambda row: add_verbosity(row['input_text'], row['target_text']),
+    axis=1
+)
+test = test.astype(str)
+
+
 model_args = T5Args()
 model_args.max_length = 100
 model_args.length_penalty = 2.5
 model_args.repetition_penalty = 1.5
 model_args.num_beams = 5
 
-model = T5Model("mt5", "all/checkpoint-15870-epoch-1", args=model_args)
+model = T5Model("mt5", "checkpoint-7935-epoch-1", args=model_args)
 
 preds = model.predict(test.input_text.values.tolist())
 test["preds"] = preds
@@ -64,4 +70,4 @@ session = boto3.Session(
     aws_secret_access_key='GcJ6N4E23VEdkRymcrFWPu24KyFUlPXw8p9ge36x',
 )
 s3 = session.resource('s3')
-s3.meta.client.upload_file(Bucket='mtacl', Key='fr_en_test_prediction', Filename='fr_en_test_prediction.csv')
+s3.meta.client.upload_file(Bucket='mtacl', Key='mt5_fr_en_test_prediction_2500', Filename='fr_en_test_prediction.csv')
